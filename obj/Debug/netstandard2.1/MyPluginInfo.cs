0ecf9a2d334c861e0286a4efc22ba8bf03ffeff0
angelcaru <angelcaru10@gmail.com>
2024-10-12 17:07:51 +0200

    gitignore files

--- obj/Debug/netstandard2.1/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace ScalingEndlessMode
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "com.angelcaru.peglin.ScalingEndlessMode";
-        public const string PLUGIN_NAME = "ScalingEndlessMode";
-        public const string PLUGIN_VERSION = "0.0.1";
-    }
-}
